3174b37f0000ec94283c3399fe95e6120fbe7f61
JmCole19 <jayemcee121@gmail.com>
2021-06-05 14:42:15 -0400

    Changes included in last commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,6 +9,21 @@ import {
 
 export default class MyNavbar extends React.Component {
     render() {
+        let items = this.props.list
+
+        const { search } = window.location;
+        const query = new URLSearchParams(search).get('s');
+
+        const filterItems = (items, query) => {
+            if (!query) {
+                return items;
+            }
+        
+            return items.filter((item) => {
+                const itemTitle = item.title.toLowerCase();
+                return itemTitle.includes(query);
+            });
+        };
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand>BurnAlong Web App</Navbar.Brand>
